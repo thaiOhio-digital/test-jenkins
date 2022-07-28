79ef7fdf32413317da0cd44c95ceb74cf8861d86
Thai Nguyen <thaind@ohio-digital.com>
2022-07-28 17:21:42 +0700

    print smt

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def generateToken():
         algorithm='HS256'
     )
     print(token)
-    print("can you clone automatically")
+    print("can you clone automatically?")
     return token.decode('utf-8')
 
 @app.get("/user")
